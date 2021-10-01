d448403f8165eaf0ee0317795e1e0eb3f70f6a40
Kbove <kbove94@gmail.com>
2021-09-30 22:33:05 -0700

    pushing the unfinished product

---
+++ README.md
@@ -0,0 +1,8 @@
+# Unit 05 Work Planner
+
+## User story 
+Life can get busy and users need a way to keep track of their daily activities and commitments
+
+### Description
+This is unfinished
+
--- script.js
+++ script.js
@@ -8,26 +8,37 @@ console.log(currentDayEl)
 //THEN I am presented with timeblocks for the business hours
 //WHEN I view the time blocks for that day
 //THEN each time block is color coded to indicate whether it is in the past, present, or future
-var currentHour;
+//var currentHour;
 //WHEN I click into a timeblock
 //THEN I can enter an event
 //WHEN I click the save button for that event
 //THEN the text for that event is saved in local storage
 var eventInputEl = document.querySelectorAll('.event-log');
+//var currentTime = (JSON.stringify(moment().format('H').toString()))
 
-function renderEvent(){
-    localStorage.getItem('');
-    $(".row").each(function (row) {
-        row.attr('id').siblings('input').val()
-})
+var currentTime = (new Date()).getHours();
 
 renderEvent();
 
-function hourPassed(){
-    var currentTime = JSON.stringify(moment().format('H').toString())
-    console.log(currentTime)
-    
-}}
+function renderEvent(){
+    $('.row').each(function () {
+        localStorage.getItem('input')
+        //localStorage.getItem('input')
+        $(this).siblings().val();
+    }
+)}
+
+$('input').each(function(){
+    if($(this).data('hour') < currentTime) {  
+        $(this).addClass("past");
+    } else if($(this).data('hour') == currentTime) { 
+        $(this).addClass("present");
+    } else {  
+        $(this).addClass("future");
+    } 
+    $(this).val(eventInputEl[$(this).data('hour')]);
+}) 
+
 
 $('.saveBtn').click(function(event){
     event.preventDefault();
@@ -38,16 +49,11 @@ $('.saveBtn').click(function(event){
     localStorage.setItem(time,input);
 })
 
+
 console.log(localStorage)
 
 /*console.log(moment().toString())
 console.log(JSON.stringify(moment().format().toString()))*/
-
-console.log(JSON.stringify(moment().format('H').toString()))
-
-
-
-
 
 /*var timeTracker = moment(currentHour, 'hh:mm:ss').diff(moment(), 'hours');
 var currentHour = dueDateInputEl.val().trim();*/
